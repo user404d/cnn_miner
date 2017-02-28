c1e7f1795da22aa7053bd1dd1a8994bec3c056d6
user404d <conduffql@gmail.com>
2017-02-28 02:57:04 -0600

    update readme and classifier

--- README.md
+++ README.md
@@ -7,26 +7,20 @@ CNN article content comparison using naive metrics.
 
 ## Generating Rankings for Article Similarities
 
-Python3.5 is a requirement for this project.
+Python3.5 is a requirement for this project and should be available via `python3.5`.
 
 Run the following commmand after cloning the repository:
 
 ```bash
-# create the virtual environment for the project
-python3.5 -m venv cnn_env
-# and once cnn_env/bin/activate is available...
-source cnn_env/bin/activate
-make
+make install
+# then
+make run_classify # <-- Assignment 2
+# -- OR --
+make run_ranking
 ```
 
 Run the following command to clean up the output:
 
 ```bash
 make clean
-```
-
-To leave the virtual environment:
-
-```bash
-deactivate
 ```
--- generate_classifications.py
+++ generate_classifications.py
@@ -10,6 +10,14 @@ from itertools import chain
 from cnn.parser import Parser
 from miner.build import Measure, Model, Ranker, VectorSpace
 
+def print_formatted_data(accs, fmeasures):
+    print(" ,Accuracy," + ",".join("{:.2f}".format(acc) for acc in accs))
+    print(" ,Average Accuracy,", sum(accs)/5)
+    print(" ,Max Accuracy,", max(accs))
+    print(" ,F-Measure," + ",".join("{:.2f}".format(fm) for fm in fmeasures))
+    print(" ,Average F-Measure,", sum(fmeasures)/5)
+    print(" ,Max F-Measure,", max(fmeasures))    
+
 def classifications():
     with open("meta/categorized_stories.csv") as lines:
         stories = {}
@@ -21,7 +29,7 @@ def classifications():
                 stories[path] = Parser.tokenize(story.read())
 
     """
-    KNN testing
+    KNN vs DTC
     """
 
     whole = Model(stories, VectorSpace.NORMALIZED)
@@ -31,19 +39,20 @@ def classifications():
     knns = [
         KNN(n_neighbors=3, metric='precomputed', n_jobs=-1),
         KNN(metric='precomputed', n_jobs=-1),
-        KNN(n_neighbors=6,metric='precomputed', n_jobs=-1),
         KNN(n_neighbors=7,metric='precomputed', n_jobs=-1),
+        KNN(n_neighbors=20,metric='precomputed', n_jobs=-1),
+        KNN(n_neighbors=80,metric='precomputed', n_jobs=-1),
+        KNN(n_neighbors=6,metric='precomputed', n_jobs=-1),
         KNN(n_neighbors=8,metric='precomputed', n_jobs=-1),
         KNN(n_neighbors=9,metric='precomputed', n_jobs=-1),
         KNN(n_neighbors=10,metric='precomputed', n_jobs=-1),
-        KNN(n_neighbors=20,metric='precomputed', n_jobs=-1),
     ]
-    dtc = DTC()
+    dtcs = [DTC(), DTC(criterion='entropy')]
 
     knn_accs = [[]] * len(knns)
     knn_fmeasures = [[]] * len(knns)
-    dtc_accs = []
-    dtc_fmeasures = []
+    dtc_accs = [[]] * len(dtcs)
+    dtc_fmeasures = [[]] * len(dtcs)
 
     for _ in range(5):
         x_train, x_test, y_train, y_test = ttsplit(
@@ -65,44 +74,104 @@ def classifications():
         ds_test = np.ones(ds_test.shape) - ds_test
 
         knn_classifiers = [ knn.fit(ds, y_train) for knn in knns ]
-        dtc_classifier = dtc.fit(x_train, y_train)
+        dtc_classifiers = [ dtc.fit(x_train, y_train) for dtc in dtcs ]
 
         knn_predictions = [ 
             knn_classy.predict(ds_test) for knn_classy in knn_classifiers 
         ]
 
-        dtc_prediction = dtc_classifier.predict(x_test)
+        dtc_predictions = [
+            dtc_classy.predict(x_test) for dtc_classy in dtc_classifiers
+        ]
         
         knn_accs = [
             accs + [knn_classy.score(ds_test,y_test)] for accs,knn_classy \
             in zip(knn_accs,knn_classifiers)
         ]
 
-        dtc_accs.append(dtc_classifier.score(x_test, y_test))
+        dtc_accs = [
+            accs + [dtc_classy.score(x_test,y_test)] for accs,dtc_classy \
+            in zip(dtc_accs,dtc_classifiers)
+        ]
         
         knn_fmeasures = [
             fmeasures + [f1(y_test, knn_prediction, average='weighted')] \
             for fmeasures,knn_prediction in zip(knn_fmeasures,knn_predictions)
         ]
 
-        dtc_fmeasures.append(f1(y_test, dtc_prediction, average='weighted'))
+        dtc_fmeasures = [
+            fmeasures + [f1(y_test, dtc_prediction, average='weighted')] \
+            for fmeasures,dtc_prediction in zip(dtc_fmeasures,dtc_predictions)
+        ]
 
-    for i,accs,fmeasures in zip(range(len(knn_accs)), knn_accs, knn_fmeasures):
-        print("[-] KNN {}".format(knns[i].n_neighbors))
-        print("Accuracy," + ",".join("{}".format(acc) for acc in accs))
-        print("Average Accuracy,", sum(accs)/5)
-        print("Max Accuracy,", max(accs))
-        print("F-Measure," + ",".join("{}".format(fm) for fm in fmeasures))
-        print("Average F-Measure,", sum(fmeasures)/5)
-        print("Max F-Measure,", max(fmeasures))
+    """
+    Plot stuff because Excel and the like are terrible...
+    """
+    inds = np.arange(5)
+    w_inds = np.arange(0,5*2,2)
+    ind_labels = tuple([str(i+1) for i in inds])
+    colors = ('r','b','g','y','m','c','#0ef0ef0e','m','#efefefef')
+    width = 0.35
 
-    print("[-] DTC")
-    print("Accuracy," + ",".join("{}".format(acc) for acc in dtc_accs))
-    print("Average Accuracy,", sum(dtc_accs)/5)
-    print("Max Accuracy,", max(dtc_accs))
-    print("F-Measure," + ",".join("{}".format(fm) for fm in dtc_fmeasures))
-    print("Average F-Measure,", sum(dtc_fmeasures)/5)
-    print("Max F-Measure,", max(dtc_fmeasures))
+    knn_accs_fig = plt.figure()
+    knn_accs_bar = knn_accs_fig.add_subplot(111)
+    knn_fmeasures_fig = plt.figure()
+    knn_fmeasures_bar = knn_fmeasures_fig.add_subplot(111)
+    for i,accs,fmeasures in zip(range(len(knns)), knn_accs, knn_fmeasures):
+        if i < 5:
+            accs_bar = knn_accs_bar.bar(w_inds+(width*i), accs, width, 
+                                        color=colors[i], label="KNN {}".format(knns[i].n_neighbors))
+            fmeasures_bar = knn_fmeasures_bar.bar(
+                w_inds+(width*i), fmeasures, width, color=colors[i], label="KNN {}".format(knns[i].n_neighbors)
+            )
+        print("[*] KNN {}".format(knns[i].n_neighbors))
+        print_formatted_data(accs, fmeasures)
+
+    knn_accs_bar.set_title("KNN Accuracy per Fold")
+    knn_accs_bar.set_xticks(w_inds+(width*2))
+    knn_accs_bar.set_xticklabels(ind_labels)
+    knn_accs_bar.set_yticks(np.arange(0, 1.0, .1))
+    knn_accs_bar.set_xlabel("Fold")
+    knn_accs_bar.set_ylabel("Accuracy")
+    knn_accs_bar.legend()
+
+    knn_fmeasures_bar.set_title("KNN F-Measure per Fold")
+    knn_fmeasures_bar.set_xticks(w_inds+(width*2))
+    knn_fmeasures_bar.set_xticklabels(ind_labels)
+    knn_fmeasures_bar.set_yticks(np.arange(0, 1.0, .1))
+    knn_fmeasures_bar.set_xlabel("Fold")
+    knn_fmeasures_bar.set_ylabel("Weighted F-Measure")
+    knn_fmeasures_bar.legend()
+
+
+    dtc_accs_fig = plt.figure()
+    dtc_accs_bar = dtc_accs_fig.add_subplot(111)
+    dtc_fmeasures_fig = plt.figure()
+    dtc_fmeasures_bar = dtc_fmeasures_fig.add_subplot(111)
+    for i,accs,fmeasures in zip(range(len(dtcs)), dtc_accs, dtc_fmeasures):
+        accs_bar = dtc_accs_bar.bar(inds+(width*i), accs, width, 
+                                    color=colors[i], label="DTC {}".format(dtcs[i].criterion))
+        fmeasures_bar = dtc_fmeasures_bar.bar(
+            inds+(width*i), fmeasures, width, color=colors[i], label="DTC {}".format(dtcs[i].criterion)
+        )
+        print("[*] DTC {}".format(dtcs[i].criterion))
+        print_formatted_data(accs, fmeasures)
+
+    dtc_accs_bar.set_title("Decision Tree Classifier Accuracy per Fold")
+    dtc_accs_bar.set_xticks(inds+(width/2))
+    dtc_accs_bar.set_xticklabels(ind_labels)
+    dtc_accs_bar.set_yticks(np.arange(0, 1.0, .1))
+    dtc_accs_bar.set_xlabel("Fold")
+    dtc_accs_bar.set_ylabel("Accuracy")
+    dtc_accs_bar.legend()
+
+    dtc_fmeasures_bar.set_title("Decision Tree Classifier F-Measure per Fold")
+    dtc_fmeasures_bar.set_xticks(inds+(width/2))
+    dtc_fmeasures_bar.set_xticklabels(ind_labels)
+    dtc_fmeasures_bar.set_yticks(np.arange(0, 1.0, .1))
+    dtc_fmeasures_bar.set_xlabel("Fold")
+    dtc_fmeasures_bar.set_ylabel("Weighted F-Measure")
+    dtc_fmeasures_bar.legend()
 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
